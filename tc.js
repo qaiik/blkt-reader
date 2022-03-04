165998f45b31e1a93444a94ad0e0e8f5fc6b29be
qaiik <82181231+qaiik@users.noreply.github.com>
2022-03-03 19:25:32 -0700

    Create tc.js

---
+++ tc.js
@@ -0,0 +1,43 @@
+let gid = prompt("Set id (for now)")
+let bdata = {};
+
+
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		bdata = JSON.parse(this.responseText);
+	}
+});
+
+xhr.open("GET", `https://api.blooket.com/api/games?gameId=${gid}`);
+
+xhr.send(data);
+let qid = ""
+function get(dat,question) {
+    for (let e of dat.questions) {
+        if (e.question === question) {
+            return e.correctAnswers[0]
+        }
+        
+    }
+}
+let cor = "";
+let h = ()=>{
+	qid = document.querySelector(".styles__questionText___10zyP-camelCase").innerText;
+	let answer = get(bdata,qid)
+	let ab = document.querySelector(".styles__answersHolder___1tefk-camelCase").children;
+	Array.from(ab).forEach((c)=>{
+		if (c.innerText == answer) {
+			c.children[0].click();
+		}
+	})
+	
+}
+
+setInterval(()=>{
+  h()
+},10}
