47291e4caec8a94a619ae5dc064f8c8b5f8729b8
qaiik <82181231+qaiik@users.noreply.github.com>
2022-03-17 21:32:34 -0600

    Update tc-alwaysyes-setpull.js

--- tc-alwaysyes-setpull.js
+++ tc-alwaysyes-setpull.js
@@ -26,13 +26,6 @@ function get(dat,question) {
     }
 }
 let cor = "";
-let h = ()=>{
-	qid = document.querySelector("#app > div > div > div:nth-child(2) > div > div > div.styles__questionContainer___3u_c9-camelCase > div > div").innerText;
-	let answer = get(bdata,qid)
-	let ab = document.querySelector(".styles__answersHolder___1tefk-camelCase").children;
-	alert(answer)
-	
-}
 
 setInterval(()=>{
 	if (document.querySelector("#app > div > div > div:nth-child(2) > div > div > div.styles__questionContainer___3u_c9-camelCase > div")) {
@@ -42,10 +35,15 @@ setInterval(()=>{
 		document.querySelector(".fa-check").click()
 	}
   
-  if (document.querySelector(".styles__guestButton___2jiD9-camelCase")) {
-    document.querySelector(".styles__guestButton___2jiD9-camelCase").click()
-  }
-  if (document.querySelector("#app > div > div > div:nth-child(2) > div > div > div.styles__container___1-bHf-camelCase")) {
-	  document.querySelector("#app > div > div > div:nth-child(2) > div > div > div.styles__container___1-bHf-camelCase").click()
-  }
+  	if (document.querySelector(".styles__guestButton___2jiD9-camelCase")) {
+    	document.querySelector(".styles__guestButton___2jiD9-camelCase").click()
+  	}
+  	if (document.querySelector("#app > div > div > div:nth-child(2) > div > div > div.styles__container___1-bHf-camelCase")) {
+	  	document.querySelector("#app > div > div > div:nth-child(2) > div > div > div.styles__container___1-bHf-camelCase").click()
+  	}
+	
 },1)
+
+window.onkeyup = ()=>{
+	get(bdata,document.querySelector("#app > div > div > div:nth-child(2) > div > div > div.styles__questionContainer___3u_c9-camelCase > div > div").innerText)
+}
