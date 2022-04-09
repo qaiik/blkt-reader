205966477a971bf9a977d0d939049ab5aa1d259f
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-09 15:39:11 -0600

    Update spamBox.js

--- misc/spamBox.js
+++ misc/spamBox.js
@@ -1,11 +1,15 @@
-function b() {
+let boxOrder = ["Safari", "Aquatic", "Bot", "Space", "Breakfast", "Medieval", "Wonderland"]
+
+let box = "Safari" //Customize this as seen in list above
+
+function b(box) {
     document.querySelector("#app > div > div > div.arts__profileRegularBody___2fvy5-camelCase > div > div.styles__boxContainer___1rYqq-camelCase > div:nth-child(3) > div.styles__middleBoxContainer___3Ld4h-camelCase > div.styles__miniBlookContainer___1GnHF-camelCase").click()
     document.querySelector("#app > div > div > div.arts__modal___VpEAD-camelCase > form > div.styles__holder___3CEfN-camelCase > div > div:nth-child(1)").click()
-    document.querySelector("#app > div > div > div.arts__modal___VpEAD-camelCase > div").click()
+    document.querySelector("#app > div > div > div.arts__profileRegularBody___2fvy5-camelCase > div > div.styles__boxContainer___1rYqq-camelCase").children[boxOrder.indexOf(box)].children[1].click()
 }
 
 setInterval(()=>{
-  b()
+  b(bn)
 },1)
 
 //Spends all your money on a box
