6140251e867362268fabe20e41d81b566a2d9b92
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-09 17:36:14 -0600

    Update no-hook-delay.js

--- fishingfrenzy/no-hook-delay.js
+++ fishingfrenzy/no-hook-delay.js
@@ -1,5 +1,6 @@
 setInterval(()=>{
   if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase > div.styles__pierBottom___1bvO_-camelCase")) {
+    //nothing special, the if statement just checks if the pier is there, signifying you are in a game, because blooket breaks if it is done before the game
     setTimeout = function (f,time){f()}
   }
 },1)
