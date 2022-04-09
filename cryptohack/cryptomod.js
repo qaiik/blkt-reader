66fe9d1a984c86b671b8f390437df53ef771b00b
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-08 21:48:26 -0600

    Update cryptomod.js

--- cryptohack/cryptomod.js
+++ cryptohack/cryptomod.js
@@ -6,7 +6,7 @@ setInterval(()=>{
   }
   
   if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase")) {
-    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase").innerHTML = `WELCOME, ${userName.toUpperCase()}<br><div style="font-size: 16px; color: white;">[CRYPTOMOD VERSION 0.0.1 BY LOGAN BIRK]</div>`
+    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase").innerHTML = `WELCOME, ${userName.toUpperCase()}<br><div style="font-size: 14px; color: white;">[CRYPTOMOD VERSION 0.0.1 BY LOGAN BIRK]</div>`
   }
   
   if (document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase")) {
@@ -22,6 +22,10 @@ setInterval(()=>{
   
   if (document.querySelector("#header > div.styles__headerRight___D5pQ1-camelCase > div")) {
     document.querySelector("#header > div.styles__headerRight___D5pQ1-camelCase > div").innerText = document.querySelector("#header > div.styles__headerRight___D5pQ1-camelCase > div").innerText.replace("₿", "Crypto: ")
+  }
+  
+  if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(2)")) {
+    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(2)").innerText = document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(2)").innerText.replace("Terminal Hacking Portal", "terminal.").replace("Terminal Hacking Portal", "terminal.")
   }
 }, 1)
   
