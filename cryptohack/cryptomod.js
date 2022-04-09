6bef5dd1b5c12b648b343707b420c3f8991ae859
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-08 21:09:16 -0600

    Update cryptomod.js

--- cryptohack/cryptomod.js
+++ cryptohack/cryptomod.js
@@ -10,11 +10,13 @@ setInterval(()=>{
   }
   
   if (document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase")) {
-    document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase").innerText = "Get Ready!\n" + document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase").innerText.replaceAll("#","+")
+    document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase").innerText = "[LOADING]\nGet Ready"
   }
   
   if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)")) {
-    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)").innerText = "[CRYPTOMOD VERSION 0.0.1 BY LOGAN BIRK]
+    setTimeout(()=>{
+      document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)").innerText = "[CRYPTOMOD VERSION 0.0.1 BY LOGAN BIRK]"
+    }, 2000}
   }
 }, 1)
   
