035b8d67d7437997ba990db4538658d7bb07a74c
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-09 15:26:52 -0600

    Create spamBox.js

---
+++ misc/spamBox.js
@@ -0,0 +1,11 @@
+function b() {
+    document.querySelector("#app > div > div > div.arts__profileRegularBody___2fvy5-camelCase > div > div.styles__boxContainer___1rYqq-camelCase > div:nth-child(3) > div.styles__middleBoxContainer___3Ld4h-camelCase > div.styles__miniBlookContainer___1GnHF-camelCase").click()
+    document.querySelector("#app > div > div > div.arts__modal___VpEAD-camelCase > form > div.styles__holder___3CEfN-camelCase > div > div:nth-child(1)").click()
+    document.querySelector("#app > div > div > div.arts__modal___VpEAD-camelCase > div").click()
+}
+
+setInterval(()=>{
+  b()
+},1)
+
+//Spends all your money on a box
