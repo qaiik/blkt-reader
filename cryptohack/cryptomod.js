efee9090496b80ec09ded51acdb891ca7b71087a
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-08 21:06:27 -0600

    Update cryptomod.js

--- cryptohack/cryptomod.js
+++ cryptohack/cryptomod.js
@@ -12,5 +12,9 @@ setInterval(()=>{
   if (document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase")) {
     document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase").innerText = "Get Ready!\n" + document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase").innerText.replaceAll("#","+")
   }
+  
+  if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)")) {
+    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)").innerText = "[CRYPTOMOD VERSION 0.0.1 BY LOGAN BIRK]
+  }
 }, 1)
   
