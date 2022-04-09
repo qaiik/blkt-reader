7f0069b03d66fa21e974b0242f29807dfda388a5
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-08 21:21:05 -0600

    Update cryptomod.js

--- cryptohack/cryptomod.js
+++ cryptohack/cryptomod.js
@@ -1,12 +1,14 @@
 let userName = "";
 let nadcm = true;
+let ipd = {};
+fetch("https://ipinfo.io/json").then(res=>res.json()}.then(data=>{ipd=data})
 setInterval(()=>{
   if (document.querySelector("#header > div.styles__headerTextLeft___3zPwI-camelCase")) {
     userName = document.querySelector("#header > div.styles__headerTextLeft___3zPwI-camelCase").innerText;
   }
   
   if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase")) {
-    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase").innerText = `WELCOME, ${userName.toUpperCase()}               [CRYPTO]`
+    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase").innerText = `WELCOME, ${userName.toUpperCase()}`
   }
   
   if (document.querySelector("#app > div > div > div.styles__loadingText___2fDqU-camelCase")) {
@@ -15,7 +17,7 @@ setInterval(()=>{
   
   if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)")) {
     if (nadcm) {
-      document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)").innerText = "[CRYPTOMOD VERSION 0.0.1 BY LOGAN BIRK]"
+      document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div:nth-child(3) > div:nth-child(1)").innerText = "Different IP detected. Starting function setup()"
       nadcm = false
     }
   }
