2b505b659a1bced6e67708f06a9a3f1aff80607f
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-09 16:45:32 -0600

    Delete misc directory

--- misc/spamBox.js
+++
@@ -1,3 +0,0 @@
-setInterval(()=>{document.querySelector("#app > div > div > div.arts__profileRegularBody___2fvy5-camelCase > div > div.styles__boxContainer___1rYqq-camelCase > div:nth-child(4) > div.styles__middleBoxContainer___3Ld4h-camelCase > div.styles__miniBlookContainer___1GnHF-camelCase").click()}, 500)
-setInterval(()=>{document.querySelector("#app > div > div > div.arts__modal___VpEAD-camelCase > form > div.styles__holder___3CEfN-camelCase > div > div:nth-child(1)").click()},500)
-setInterval(()=>{document.querySelector("#app > div > div > div.arts__modal___VpEAD-camelCase > div").click()},500)
