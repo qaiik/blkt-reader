8926bce3417cb068c62394f1f5200f1b734388f1
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-09 16:34:36 -0600

    Update fstr.js

--- fishingfrenzy/fstr.js
+++ fishingfrenzy/fstr.js
@@ -14,7 +14,7 @@ setInterval(()=>{
   if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase > div.styles__pierBottom___1bvO_-camelCase")) {
     setTimeout = function (f,time){f()}
   }
-},100)
+},1)
               
 
 
