6ec01a74851bfeb11065a3f878b5e688633af103
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-08 20:58:48 -0600

    Create cryptomod.js

---
+++ cryptohack/cryptomod.js
@@ -0,0 +1,12 @@
+let userName = "";
+
+setInterval(()=>{
+  if (document.querySelector("#header > div.styles__headerTextLeft___3zPwI-camelCase")) {
+    userName = document.querySelector("#header > div.styles__headerTextLeft___3zPwI-camelCase").innerText;
+  }
+  
+  if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase")) {
+    document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase.styles__body___12LVw-camelCase > div.styles__terminal___-b2QX-camelCase > div.styles__introHeader___Dzfym-camelCase").innerText = `WELCOME, ${userName.toUpperCase()}`
+  }
+}
+  
