7c8eeab6164294d04444085f677912467b1b3fc4
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-09 16:34:50 -0600

    Create no-hook-delay.js

---
+++ fishingfrenzy/no-hook-delay.js
@@ -0,0 +1,5 @@
+setInterval(()=>{
+  if (document.querySelector("#app > div > div > div.arts__regularBody___1TM6E-camelCase > div.styles__pierBottom___1bvO_-camelCase")) {
+    setTimeout = function (f,time){f()}
+  }
+},1)
