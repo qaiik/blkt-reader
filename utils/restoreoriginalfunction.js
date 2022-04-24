f5a91b5c132fe5f3982f67a7b7bb0bb1280c73ac
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-23 22:40:05 -0600

    Update restoreoriginalfunction.js

--- utils/restoreoriginalfunction.js
+++ utils/restoreoriginalfunction.js
@@ -2,6 +2,7 @@ let ifrof = document.createElement("iframe")
 ifrof.style.visibility = "hidden"
 ifrof.style.width = "0px"
 ifrof.style.height = "0px"
+ifrof.scrolling = "no"
 document.body.appendChild(ifrof)
 function restoreFunction(name) {
   return ifrof.contentWindow[name]
