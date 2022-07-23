f39e93b2a0e8d117c8c00a6a253e5974ad8f30cd
qaiik <82181231+qaiik@users.noreply.github.com>
2022-07-23 15:16:02 -0600

    Update blooket.js

--- utils/blooket.js
+++ utils/blooket.js
@@ -1,9 +1,17 @@
 // in progress - blooket hacking library
 let kit = {};
 
-function _$kitreacthandler() {
+kit._$krh = function _$kitreacthandler() {
     return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
 }
 
+function unalloc(name) {
+    delete kit[name]
+}
 
-[kit.react, kit.hack, kit.handler, kit.reactHandler] = Array(4).fill(_$kitreacthandler)
+[kit.react, kit.hack, kit.handler, kit.reactHandler] = Array(4).fill(kit._$krh);
+unalloc("_$krh");
+
+kit.mode = () => location.pathname.split("/").at(-1);
+
+
